79b7b5dd890aa6b5f77b4d2da2fb1e47290c2693
chrisdews <cldews1@gmail.com>
2021-04-08 12:52:35 +0100

    moved all variables to env

--- src/components/golfDraft/GolfDraft.js
+++ src/components/golfDraft/GolfDraft.js
@@ -13,14 +13,13 @@ import apiMock from "../../hardcodedContent/players";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "golf-draft-1e6c3.firebaseapp.com",
-  databaseURL:
-    "https://golf-draft-1e6c3-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "golf-draft-1e6c3",
-  storageBucket: "golf-draft-1e6c3.appspot.com",
-  messagingSenderId: "280689716769",
-  appId: "1:280689716769:web:f1bbb2a98700e3c6aebd60",
-  measurementId: "G-NQG637CCFY",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
