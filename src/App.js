039eb8df6fbb8e8cc580e329de37f36a41ded786
chrisdews <cldews1@gmail.com>
2021-05-22 23:21:39 +0100

    improved getSelectedPlayers logic refresh now persists whosTurn and pickNo - fixed use mock data bug where it was still calling the live api oops

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import "./App.css";
-import GolfDraft from "./components/golfDraft";
+import HomeLayout from "./components/homeLayout";
 
 import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
-      <GolfDraft />
+      <HomeLayout />
     </div>
   );
 }
---
+++ src/components/homeLayout/HomeLayout.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./HomeLayout.css"
+import GolfDraft from '../golfDraft'
+
+import { Layout, Menu, Breadcrumb } from "antd";
+const { Header, Content, Footer } = Layout;
+
+
+function HomeLayout() {
+  return (
+    <div>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+
+
+          <div className="site-layout-content">
+            <GolfDraft />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          GolfDraft ©2021 Created by Chris Dews & Xander Johnston
+        </Footer>
+      </Layout>
+    </div>
+  );
+}
+
+Layout.propTypes = {};
+
+export default HomeLayout;
+
+
